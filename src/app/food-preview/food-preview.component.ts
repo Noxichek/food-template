5f06138565cdd5fcd613746399fd44ca451d909a
Nikita Dachkovskiy <n.dachkovskiy@kmd24.com>
2022-11-29 11:25:12 +0300

    add styles

---
+++ src/app/food-preview/food-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-preview',
+  templateUrl: './food-preview.component.html',
+  styleUrls: ['./food-preview.component.scss']
+})
+export class FoodPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
